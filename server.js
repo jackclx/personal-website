ff06136c9b76c6706166703a6e77c7be91696e15
jackclx <chenlexinlt@gmail.com>
2024-06-07 23:14:38 +0700

    hide my email and password

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "chenlexinlt@gmail.com",
-    pass: "661063lt"
+    user: "******",
+    pass: "******"
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "chenlexinlt@gmail.com",
+    to: "******",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
