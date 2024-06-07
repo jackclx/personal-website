f47228601e50df7d2d1c886a4fc19edabcf8291a
jackclx <chenlexinlt@gmail.com>
2024-06-07 23:23:03 +0700

    created contact api

--- server.js
+++ server.js
@@ -1,52 +1,34 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-// server used to send send emails
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const { firstName, lastName, email, phone, message } = req.body;
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "******",
-    pass: "******"
-  },
-});
+    const contactEmail = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      },
+    });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
+    const mail = {
+      from: `${firstName} ${lastName}`,
+      to: process.env.EMAIL_USER,
+      subject: "Contact Form Submission - Portfolio",
+      html: `<p>Name: ${firstName} ${lastName}</p>
+             <p>Email: ${email}</p>
+             <p>Phone: ${phone}</p>
+             <p>Message: ${message}</p>`,
+    };
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "******",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
+    try {
+      await contactEmail.sendMail(mail);
+      res.status(200).json({ code: 200, status: "Message Sent" });
+    } catch (error) {
+      res.status(500).json({ error });
     }
-  });
-});
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
