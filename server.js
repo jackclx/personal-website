bac65641869277374345c4899448245cf5f66f5b
Marcus Ang <marcusajh0802@gmail.com>
2024-06-09 11:10:34 +0700

    runs locally

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
-module.exports = async (req, res) => {
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow only your frontend origin
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.post('/contact', async (req, res) => {
   if (req.method === 'POST') {
     const { firstName, lastName, email, phone, message } = req.body;
 
@@ -13,7 +30,7 @@ module.exports = async (req, res) => {
     });
 
     const mail = {
-      from: `${firstName} ${lastName}`,
+      from: `${firstName} ${lastName} <${email}>`,
       to: process.env.EMAIL_USER,
       subject: "Contact Form Submission - Portfolio",
       html: `<p>Name: ${firstName} ${lastName}</p>
@@ -31,4 +48,9 @@ module.exports = async (req, res) => {
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
-};
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("http://localhost:4000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
@@ -65,7 +65,7 @@ export const Contact = () => {
                       <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                      <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
